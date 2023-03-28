b6672567390ddf227447752205ff6be01e81e15f
hippyshiro <67040024+hippyshiro@users.noreply.github.com>
2023-03-29 12:19:03 +1300

    Key Asset and Functions

---
+++ BorisProject/Assets/Scripts/Key.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public GameObject Player_Obj; 
+    public GameManager GM_Obj; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player_Obj = GameObject.Find("Player");
+        GM_Obj = GameObject.Find("GameTrigger").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            GM_Obj.SetKey(true);
+        }
+    }
+}
